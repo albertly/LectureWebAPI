ae813d3379d76ab4b8fc3351ee7a232a0d2aa019
albertly <albertly@yandex.ru>
2021-01-14 19:32:43 +0200

    Add Client

---
+++ ClientWeb/Program.cs
@@ -0,0 +1,68 @@
+﻿using Polly;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ClientWeb
+{
+    class Program
+    {
+
+        static void  Main ( string[] args )
+        {
+            var policy = Policy.Handle<HttpRequestException>( )
+                .WaitAndRetry(4,
+                retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                ( exception, timeSpan, retryCount, context ) =>
+                {
+                    Console.WriteLine("timeSpan:" + timeSpan);
+                    Console.WriteLine("retryCount:" + retryCount);
+                });
+
+            HttpClient _client = new HttpClient( );
+            var baseURL = "https://localhost:5002/";
+
+            _client.BaseAddress = new Uri(baseURL);
+            _client.DefaultRequestHeaders.Accept.Clear( );
+            //_client.DefaultRequestHeaders.Accept.Add( );
+            new MediaTypeWithQualityHeaderValue("application/json");
+
+            try
+            {
+                policy.Execute(() => MakeCall(_client).GetAwaiter( ).GetResult( ));
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(-1);
+            }
+            
+            
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static async Task<string> MakeCall ( HttpClient _client)
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(
+                        @"/WeatherForecast");
+                response.EnsureSuccessStatusCode( );
+
+                // return URI of the created resource.
+                var result = response.Content.ReadAsStringAsync( ).GetAwaiter( ).GetResult( );
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            return "1";
+        }
+       
+    }
+}
